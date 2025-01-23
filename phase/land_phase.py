355dbc922d3c717f9c400f3ee7e87d9ab8e5861f
Ninzinpan <monsutokazuki01@gmail.com>
2025-01-23 21:04:05 +0900

    def land_pressure(self)を別の方法に変更しました
    このファイルに問題が起きたら真っ先にここを疑ってください。

--- phase/land_phase.py
+++ phase/land_phase.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append("/home/pi/TANE2025/")
 from module import class_pressure
-from phase import subthread
+#from phase import subthread
 import time
 
 class Land:
@@ -11,8 +11,9 @@ class Land:
         else:                    self.get_pressure = get_pressure
 
         if subth == None:
-            self.subth = subthread.Subthread(pressure=self.get_pressure)
-            self.subth.run()
+            pass
+            #self.subth = subthread.Subthread(pressure=self.get_pressure)
+            #self.subth.run()
         else:                    self.subth = subth
 
         self.sky=sky   #上空まで上がったか判定するときの大気圧の変化の閾値 要調整
@@ -32,6 +33,7 @@ class Land:
     #上空に上がったか判定
     def sky_pressure(self):
         i=0
+        
         while i<=10:
             self.get_pressure.read() #毎回pressure更新
             print(self.get_pressure.pressure)
@@ -42,31 +44,58 @@ class Land:
                 i=0 #やり直し
                 print("yet")
             time.sleep(0.5)
+            #ビョウソク6m
         print("sky")
     
     #地上まで降りたか判定
     def land_pressure(self):
+      
+
+        n = 0
         i=0
         start_sky_time = time.time()
-        limit_sky_time = 15*60 # 上空検知してから15分経過したら強制的に着地判定
-        while i<=10 and (time.time() - start_sky_time) < limit_sky_time:
+        limit_sky_time = 999999999 # 上空検知してから15分経過したら強制的に着地判定
+        while   (time.time() - start_sky_time< limit_sky_time):
             self.get_pressure.read() #毎回pressure更新
             print(self.get_pressure.pressure)
             if self.start_pressure-self.get_pressure.pressure < self.land: #閾値暫定
                 print(i)
                 i=i+1
+
             else:
                 i=0 #やり直し
                 print("yet")
-            time.sleep(0.5)
+            if n == 0:
+                now_pressure = self.get_pressure.pressure
+                time.sleep(5)
+                self.get_pressure.read()
+                if self.get_pressure.pressure - now_pressure > 0.2:
+                    n += 1
+                else:
+                    n = 0
+                    print("fall_yet")
+            if n >=1:
+                start_sky_time = time.time()
+                limit_sky_time = 15*60 # 上空検知をスタート
+                n = -1
+                print("fall start")
+
+            
+            time.sleep(0.1)
+
+            
+            
         print("land")
 
+
+  
+
     def run(self):
-        self.subth.phase=0
-        self.sky_pressure()
-        self.subth.record(comment="sky")
+        #self.subth.phase=0
+        #self.sky_pressure()
+        #self.subth.record(comment="sky")
         self.land_pressure()
-        self.subth.record(comment="land")
+        #self.subth.record(comment="land")
 
 def main(sky=1.0, land=0.1): #上空判定，地上判定の閾値
     try:
