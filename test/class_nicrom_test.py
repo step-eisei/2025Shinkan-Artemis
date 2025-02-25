8651e2cef8ad6a022b09419f30eebcbc463c9710
“STEPeisei” <step.eisei@gmail.com>
2025-02-25 16:59:09 +0900

    test resistance burnout process

---
+++ test/class_nicrom_test.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import time
+
+class Nicrom():
+    def __init__(self, pin=29):
+        self.pin = pin
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin,  GPIO.OUT)
+
+    def heat(self, t=3):
+        GPIO.output(self.pin, False)
+        time.sleep(30)
+        GPIO.output(self.pin, True)
+        time.sleep(t)
+        GPIO.output(self.pin, False)
+    
+    def end(self):
+        GPIO.output(self.pin, False)
+
+def main():
+    nicrom = Nicrom()
+
+    try:
+        print("start")
+        nicrom.heat(t=10)
+
+        nicrom.end()
+        print("end")
+    except:
+        nicrom.end()
+        print("\nerror.")
+
+if __name__ == "__main__":
+    main()
